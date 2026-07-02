326702c0797d867165de059201ae7edf268f8e8d
Matheus <matheusgubert88@gmail.com>
2026-07-02 18:49:47 -0300

    finalizado

--- sprint7/src/app/features/home/home.component.ts
+++ sprint7/src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 
@@ -12,6 +12,7 @@ import { Router, RouterLink } from '@angular/router';
 
 export class HomeComponent implements OnInit {
   userName = '';
+  sidebarOpen = false;
 
   constructor(private router: Router) {}
 
@@ -25,11 +26,24 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar(): void {
+    this.sidebarOpen = false;
+  }
+
+  // Fecha a sidebar ao pressionar ESC
+  @HostListener('document:keydown.escape')
+  onEscapeKey(): void {
+    this.closeSidebar();
+  }
+
   onLogout(): void {
     if (typeof window !== 'undefined' && window.localStorage) {
       localStorage.removeItem('user');
     }
     this.router.navigate(['/login']);
   }
-  
-};
+}
